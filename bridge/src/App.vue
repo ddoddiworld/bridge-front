3d0c93149425fa2bc947991423f13aac782d53ca
장소영 <syjang@intocns.com>
2024-08-06 21:17:15 +0900

    [test] 간단한 api 통신

--- bridge/src/App.vue
+++ bridge/src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div v-for="(item, index) in list" :key="index" class="api-test">
+    <p class="api-test_id">{{ item.id }}</p>
+    <p class="api-test_name">{{ item.name }}</p>
+    <p class="api-test_email">{{ item.email }}</p>
+    <p class="api-test_phone">{{ item.phone }}</p>
+    <ul class="api-test_address">
+      <li>{{ item.city }}</li>
+      <li>{{ item.district }}</li>
+    </ul>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue'
+
+/** list: data */
+const list = ref([])
+
+/** api 호출 */
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://koreanjson.com/users')
+    const data = await response.json()
+    list.value = data
+  } catch (error) {
+    console.error('error :', error)
+  }
+}
+
+/** mounted */
+onMounted(() => {
+  fetchData()
+})
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.api-test {
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+}
+.api-test_id {
+  font-size: 20px;
+  font-weight: bold;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.api-test_name {
+  font-size: 16px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.api-test_address {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  color: darkgray;
+}
+.api-test_address li {
+  margin-right: 5px;
 }
 </style>
